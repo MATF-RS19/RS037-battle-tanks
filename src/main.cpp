9a5a1feaf7ab2233ddae74f172cce1d40153b817
TodorovTijana <mi14338@alas.matf.bg.ac.rs>
2018-12-28 03:11:13 +0100

    Uradjeno:
    Promenjen je ceo kod, iscrtavanje i iskljucene rotacije.
    Sada se polje, i igraci crtaju u ravni xz, a ne u xy kao ranije kada
     sam rotirala.
    Ubacena su osvetljenja, i koriscenje misa.
    
    Plan za dalje:
    
    Srediti sliku, posto nestaje nakon aktiviranja igre.
    Ukljuciti prekid igrice kada neko od igraca dodje do health-a 10.
    Namestiti lepo rotiranje tenka tako da puca, da rotacija nisana ide
    samo do 90stepeni.

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,12 @@
 #define TIMER_ID 0
 #define TIMER_INTERVAL 100
 #define FILENAME "universe.bmp"
+#define POMERAJ 0.02
 
+int up=1;
+double up_vector_y_cor=0;
+static int mouse_x, mouse_y;
+static float matrix[16];
 
 void on_display(void);
 void initialize(void);
@@ -13,6 +18,9 @@ void on_reshape(int width, int height);
 void on_keyboard(unsigned char key, int x, int y);
 void on_timer(int value);
 void prepreka(void);
+static void on_mouse(int button, int state, int x, int y);
+static void on_motion(int x, int y);
+
 
 int width_window = 700;
 int height_window = 500;
@@ -22,48 +30,53 @@ GLuint slika_pozadine;
 
 void initialize(void){
   glClearColor(0,0,0,0);
+  glGetFloatv(GL_MODELVIEW_MATRIX, matrix);
 
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
-  gluLookAt(0,1,-1.75,0,0,0,0,0,1);
-  	
-  GLfloat light_position[] = {0,0,1,0};
-  GLfloat ambient_light[] = {0,0,0,1};
-  GLfloat diffuse_light[] = {0.5,0,0,1};
-  GLfloat specular_light[] = {0.7,0,0,1};
-
-  GLfloat ambient_coeffs[] = {0.6,0.1,0,1};
-  GLfloat diffuse_coeffs[] = {0.2,0.2,0.2,1};
-  GLfloat specular_coeffs[] = {0.1,0.1,0.1,1};
-
-  GLfloat shininess = 20;
-
-  glLightfv(GL_LIGHT1,GL_POSITION,light_position);
-  glLightfv(GL_LIGHT1,GL_AMBIENT,ambient_light);
-  glLightfv(GL_LIGHT1,GL_DIFFUSE,diffuse_light);
-  glLightfv(GL_LIGHT1,GL_SPECULAR,specular_light);
-
-  glMaterialfv(GL_FRONT,GL_AMBIENT,ambient_coeffs);
-  glMaterialfv(GL_FRONT,GL_DIFFUSE,diffuse_coeffs);
-  glMaterialfv(GL_FRONT,GL_SPECULAR,specular_coeffs);
-  glMaterialf(GL_FRONT,GL_SHININESS,shininess);
-
+  
+    slika_pozadine=SOIL_load_OGL_texture("deixis_darksky.png",SOIL_LOAD_AUTO,SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
+  if(slika_pozadine==0){
+   std::cout << "Nije ucitana slika" << std::endl;   
+  } 
+  glEnable(GL_DEPTH_TEST);
 }
 
-void on_keyboard(unsigned char key, int x, int y){
-  switch (key) {
-    case 27:
-      exit(EXIT_SUCCESS);
-      break;
-    case 's':
-    case 'S':
-      if(!animation_ongoing){
-	animation_ongoing = 1;
-	glutTimerFunc(TIMER_INTERVAL, on_timer, TIMER_ID);
-      std::cout << "pritisnuto s" << std::endl;
-      }
-      break;
-  }
+static void on_mouse(int button, int state, int x, int y)
+{
+    mouse_x = x;
+    mouse_y = y;
+}
+
+
+
+static void on_motion(int x, int y)
+{
+    /* Promene pozicije pokazivaca misa. */
+    int delta_x, delta_y;
+
+    /* Izracunavaju se promene pozicije pokazivaca misa. */
+    delta_x = x - mouse_x;
+    delta_y = y - mouse_y;
+    
+
+      
+    /* Pamti se nova pozicija pokazivaca misa. */
+    mouse_x = x;
+    mouse_y = y;
+
+    /* Izracunava se nova matrica rotacije. */
+    glMatrixMode(GL_MODELVIEW);
+    glPushMatrix();
+        glLoadIdentity();
+        //glRotatef(180 * (float) delta_x / width_window, 0, 1, 0);
+        glRotatef(180 * (float) delta_y / height_window, 0, 0, 1);
+        glMultMatrixf(matrix);
+
+        glGetFloatv(GL_MODELVIEW_MATRIX, matrix);
+    glPopMatrix();
+    
+    glutPostRedisplay();
 }
 
 
@@ -76,7 +89,7 @@ void on_reshape(int width, int height){
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
   gluPerspective(53,width_window/(float)height_window,1, 15);
-
+  
 }
 
 class floor{
@@ -85,15 +98,17 @@ public:
     : m_x(x), m_y(y), m_z(z)
     {};
     
-    void floor_draw(){
-      
-      glBegin(GL_POLYGON);
+    void floor_draw(){   
+
+      glPushMatrix();
       glColor3f(0,0.5,0.5);
+      glBegin(GL_POLYGON);
 	glVertex3f(-m_x,m_y,m_z);
-	glVertex3f(-m_x,-m_y,m_z);
-	glVertex3f(m_x,-m_y,m_z);
+	glVertex3f(-m_x,m_y,-m_z);
+	glVertex3f(m_x,m_y,-m_z);
 	glVertex3f(m_x,m_y,m_z);	    
       glEnd(); 
+      
     }
     
 private:
@@ -114,91 +129,61 @@ public:
     : m_x(x), m_y(y), m_z(z){};
   
     void gamer1_draw(void){
-
-      
-    GLfloat light_ambient[] = {0.6,0.5,0.1,0,0};
-    GLfloat light_diffuse[] = {0.4,0.2,0.8,0};
-    GLfloat light_specular[] = {0.8,1,0,0.5};
-    GLfloat light_position[] = {-0.5,-0.5,-1,0};
-
-    GLfloat model_ambient[] = {0.9,0, 0, 0};
-    GLfloat model_diffuse[] = {0.1,0.2,0.8,0};
-    GLfloat model_specular[] = {0.75,0.6,0,1};
-    GLfloat model_shininess = 20;
-
-   glLightfv(GL_LIGHT0,GL_AMBIENT,light_ambient);
-   glLightfv(GL_LIGHT0,GL_DIFFUSE,light_diffuse);
-   glLightfv(GL_LIGHT0,GL_SPECULAR,light_specular);
-   glLightfv(GL_LIGHT0,GL_POSITION,light_position);
-      
-   glMaterialfv(GL_FRONT,GL_AMBIENT,model_ambient);
-   glMaterialfv(GL_FRONT,GL_DIFFUSE,model_diffuse);
-   glMaterialfv(GL_FRONT,GL_SPECULAR,model_specular);
-   glMaterialf(GL_FRONT,GL_SHININESS,model_shininess);
-
-
-
-
       //CRVENI
-      glTranslatef(m_x, m_y, m_z);
       glPushMatrix();
-	glRotatef(90,0,1,0);
+      glTranslatef(m_x, m_y, m_z);
+      glPushMatrix();      
 	glPushMatrix();
 	  glColor3f(0.8,0,0);
-	  glScalef(0.6,1,1.5);
+	  glScalef(1.5,0.6,1);
 	  glutSolidCube(0.2);
 	glPopMatrix();
 	glPushMatrix();
-	  glEnable(GL_LIGHTING);
-	  glEnable(GL_LIGHT0);
-
 	  glColor3f(0.6,0,0);
-	  glTranslatef(0.1,0.02,0);
-	  glScalef(0.4,0.6,1);
+	  glTranslatef(0,0.1,0.02);
+	  glScalef(1,0.4,0.5);
 	  glutSolidCube(0.2);
-	  glDisable(GL_LIGHT0);
-	  glDisable(GL_LIGHTING);
 	glPopMatrix();
 	glPushMatrix();
 	  glColor3f(0,0,0);
-	  glTranslatef(0.15,0.02,0.1);
-	  glScalef(0.1,0.1,1.5);
+//	  glMultMatrixf(matrix);
+	  glTranslatef(-0.2,0.15,0.02);
+	  glScalef(1.5,0.1,0.1);
 	  glutSolidCube(0.2);
 	glPopMatrix();
-      glPopMatrix();	
+      glPopMatrix();
       glTranslatef(-m_x, -m_y, -m_z);
+      glPopMatrix();
       }
       
       double health(){
 	glPushMatrix();
-	  glRotatef(-90,1,0,0);
-	  glTranslatef(-0.5,1,0);
+	  glTranslatef(1,-1,0);
 	  glBegin(GL_POLYGON);
-	    glColor3f(1,0,0);
-	    glVertex3f(-0.3,0.03,0);
-	    glVertex3f(-0.3,-0.03,0);
 	    glColor3f(0,1,0);
-	    glVertex3f(0.3,-0.03,0);
-	    glVertex3f(0.3,0.03,0);
+	    glVertex3f(-0.5,0.05,0);
+	    glVertex3f(-0.5,-0.05,0);
+	    glColor3f(1,0,0);
+	    glVertex3f(0.5,-0.05,0);
+	    glVertex3f(0.5,0.05,0);
 	  glEnd();
 	glPopMatrix();
 		
 	glPushMatrix();
-	  glRotatef(-90,1,0,0);
-	  glTranslatef(-0.5,1,0);
+	  glTranslatef(1,-1,0);
 	  glBegin(GL_POLYGON);
 	    glColor3f(0,0,0);
-	    glVertex3f(-0.3,0.03,0);
-	    glVertex3f(-0.3,-0.03,0);
-	    glVertex3f(-0.3+0.3*m_health_red,-0.03,0);
-	    glVertex3f(-0.3+0.3*m_health_red,0.03,0);
+	    glVertex3f(-0.5,0.05,0);
+	    glVertex3f(-0.5,-0.05,0);
+	    glVertex3f(-0.5+0.5*m_health_red,-0.05,0);
+	    glVertex3f(-0.5+0.5*m_health_red,0.05,0);
 	  glEnd();
 	glPopMatrix();
 	    
 	return m_health_red;
       }
 
-private:  
+ 
     double m_x;
     double m_y;
     double m_z;
@@ -216,126 +201,116 @@ public:
     : m_x(x), m_y(y), m_z(z){};
   
     void gamer2_draw(void){
-      
-    GLfloat light_ambient[] = {0.6,0.5,0.1,0,0};
-    GLfloat light_diffuse[] = {0.4,0.2,0.8,0};
-    GLfloat light_specular[] = {0.8,1,0,0.5};
-    GLfloat light_position[] = {0.5,-0.5,-1,0};
-
-    GLfloat model_ambient[] = {0,0, 0.9, 0};
-    GLfloat model_diffuse[] = {0.1,0.2,0.8,0};
-    GLfloat model_specular[] = {0.75,0.6,0,1};
-    GLfloat model_shininess = 20;
-
-   glLightfv(GL_LIGHT0,GL_AMBIENT,light_ambient);
-   glLightfv(GL_LIGHT0,GL_DIFFUSE,light_diffuse);
-   glLightfv(GL_LIGHT0,GL_SPECULAR,light_specular);
-   glLightfv(GL_LIGHT0,GL_POSITION,light_position);
-      
-   glMaterialfv(GL_FRONT,GL_AMBIENT,model_ambient);
-   glMaterialfv(GL_FRONT,GL_DIFFUSE,model_diffuse);
-   glMaterialfv(GL_FRONT,GL_SPECULAR,model_specular);
-   glMaterialf(GL_FRONT,GL_SHININESS,model_shininess);
-
+ 
       //PLAVI
-      glTranslatef(m_x, m_y, m_z);
       glPushMatrix();
-	glRotatef(180,0,0,1);
-	glRotatef(90,0,1,0);
+      glTranslatef(m_x, m_y, m_z);
+      glPushMatrix();      
 	glPushMatrix();
 	  glColor3f(0,0,0.8);
-	  glScalef(0.6,1,1.5);
+	  glScalef(1.5,0.6,1);
 	  glutSolidCube(0.2);
 	glPopMatrix();
 	glPushMatrix();
-	  glEnable(GL_LIGHT0);
-	  glEnable(GL_LIGHTING);
 	  glColor3f(0,0,0.6);
-	  glTranslatef(0.1,0.02,0);
-	  glScalef(0.4,0.6,1);
+	  glTranslatef(0,0.1,0.02);
+	  glScalef(1,0.4,0.5);
 	  glutSolidCube(0.2);
-	  glDisable(GL_LIGHT0);
-      glDisable(GL_LIGHTING);
-     
 	glPopMatrix();
+	/*za pucanje*/
 	glPushMatrix();
-	  glColor3f(0,0,0);
-	  glTranslatef(0.15,0.02,0.1);
-	  glScalef(0.1,0.1,1.5);
+	  glColor3f(0,1,0);
+	  glPushMatrix();
+	  glTranslatef(0,0.15,0.02);
+	  glScalef(0.5,0.3,0.1);
+	  glutSolidCube(0.2);
+	  glPopMatrix();
+	  glMultMatrixf(matrix);
+	  glTranslatef(0.2,0.15,0.02);
+	  glScalef(1.5,0.1,0.1);
 	  glutSolidCube(0.2);
 	glPopMatrix();
-      glPopMatrix();	
+      glPopMatrix();
       glTranslatef(-m_x, -m_y, -m_z);
-
+      glPopMatrix();
       }
       
       double health(){
 	glPushMatrix();
-	  glRotatef(-90,1,0,0);
-	  glTranslatef(0.5,1,0);
+	  glTranslatef(-1,-1,0);
 	  glBegin(GL_POLYGON);
-	    glColor3f(0,1,0);
-	    glVertex3f(-0.3,0.03,0);
-	    glVertex3f(-0.3,-0.03,0);
 	    glColor3f(1,0,0);
-	    glVertex3f(0.3,-0.03,0);
-	    glVertex3f(0.3,0.03,0);
+	    glVertex3f(-0.5,0.05,0);
+	    glVertex3f(-0.5,-0.05,0);
+	    glColor3f(0,1,0);
+	    glVertex3f(0.5,-0.05,0);
+	    glVertex3f(0.5,0.05,0);
 	  glEnd();
 	glPopMatrix();
 	
 	glPushMatrix();
-	  glRotatef(-90,1,0,0);
-	  glTranslatef(0.5,1,0);
+	  glTranslatef(-1,-1,0);
 	  glBegin(GL_POLYGON);
 	    glColor3f(0,0,0);
-	    glVertex3f(-0.3,0.03,0);
-	    glVertex3f(-0.3,-0.03,0);
-	    glVertex3f(-0.3 + 0.3*m_health_blue,-0.03,0);
-	    glVertex3f(-0.3 + 0.3*m_health_blue,0.03,0);
+	    glVertex3f(-0.5,0.05,0);
+	    glVertex3f(-0.5,-0.05,0);
+	    glVertex3f(-0.5 + 0.5*m_health_blue,-0.05,0);
+	    glVertex3f(-0.5 + 0.5*m_health_blue,0.05,0);
 	  glEnd();
 	glPopMatrix();
 	  return m_health_blue;
       }
   
-private:
+//private:
   
     double m_x;
     double m_y;
     double m_z;
     double m_health_blue = 0;
 };
-
+/* zid */
 class obstacle{
 public:
-  
   obstacle()
   {}
   
   obstacle(double x, double y, double z)
   :m_x(x), m_y(y), m_z(z)
   {}
-
+  
+/* iscrtavanje zida i njegovo pomeranje gore-dole */
   void obstacle_draw(){
-    
+    if(up){
+      m_y+=up_vector_y_cor;
+      if(m_y>.5){	
+	up=0;
+      }
+    }
+    else{
+      m_y-=up_vector_y_cor;
+      if(m_y<=0.1){
+	up=1;
+      }
+    }
     glPushMatrix(); 
       glColor3f(0.2,0.2,0.2);
-      glTranslatef(m_x, m_y, m_z);
-      glScalef(0.4,5,3);
+      glTranslatef(m_x, m_y+0.1, m_z);
+      glScalef(0.4,1,5);
       glutSolidCube(0.2);
     glPopMatrix();  
   }
   
   
-private:
+//private:
   double m_x;
   double m_y;
   double m_z;
 
 };
 
-gamer1 gRed(-0.6,0,0);
-gamer2 gBlue(0.6,0,0);
-floor floor(1,1,0);
+gamer1 gRed(0.6,0.06,0.5);
+gamer2 gBlue(-0.6,0.06,0.5);
+floor floor(1,0,1);
 obstacle obstacle(0,0,0);
 
 int main(int argc, char* argv[]){
@@ -351,15 +326,13 @@ int main(int argc, char* argv[]){
   glutDisplayFunc(on_display);
   glutReshapeFunc(on_reshape);
   glutKeyboardFunc(on_keyboard);
-  glutTimerFunc(TIMER_INTERVAL,on_timer,TIMER_ID);
+  glutMouseFunc(on_mouse);
+  glutMotionFunc(on_motion);
   
-  slika_pozadine=SOIL_load_OGL_texture("deixis_darksky.png",SOIL_LOAD_AUTO,SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
-  if(slika_pozadine==0){
-   std::cout << "Nije ucitana slika" << std::endl;   
-  }  
+  mouse_x = 0;
+  mouse_y = 0;
   
   glutMainLoop();
-
   return 0;
 }
 
@@ -369,23 +342,23 @@ void on_display(void){
   glShadeModel(GL_SMOOTH);  
    glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
-  gluLookAt(0,1,-1.5,0,0,1,0,0,1);
-  glRotatef(90,1,0,0);
+  gluLookAt(0,0.5,2,0,0,0,0,1,0);
  
+
   glPushMatrix();   
     glEnable(GL_TEXTURE_2D);
-    glRotatef(-60, 1, 0, 0); 
+    glRotatef(0, 1, 0, 0); 
     glBegin(GL_QUADS);
       glBindTexture(GL_TEXTURE_2D,slika_pozadine);
       glNormal3f(0, 1, 0);
       glTexCoord2f(0, 0);
-      glVertex3f(-2, -2, 0);
+      glVertex3f(-3, -2, 0);
       glTexCoord2f(1, 0);
-      glVertex3f(2, -2, 0);
+      glVertex3f(3, -2, 0);
       glTexCoord2f(1, 1);
-      glVertex3f(2, 2, 0);
+      glVertex3f(3, 2, 0);
       glTexCoord2f(0, 1);
-      glVertex3f(-2, 2, 0);
+      glVertex3f(-3, 2, 0);
     glEnd();
     glDisable(GL_TEXTURE_2D);
   glPopMatrix();     
@@ -394,12 +367,9 @@ void on_display(void){
   obstacle.obstacle_draw();
   gRed.gamer1_draw();
   gBlue.gamer2_draw();
+  
   gRed.health();
   gBlue.health();
-  /*
-  if((gRed.health() <= 9) && (gBlue.health() <= 9)){
-    glutTimerFunc(TIMER_INTERVAL,on_timer, TIMER_ID);    
-  }*/
   
   glutSwapBuffers();
 }
@@ -408,10 +378,49 @@ void on_timer(int value){
   if(value!=TIMER_ID){
     return;
   }
-  
+  up_vector_y_cor=0.01;
   if(animation_ongoing){
-    obstacle.obstacle_draw();
+  
     glutTimerFunc(TIMER_INTERVAL,on_timer, TIMER_ID);
   }
-  
+  glutPostRedisplay();
+ 
+}
+void on_keyboard(unsigned char key, int x, int y){
+  switch (key) {
+    case 27:
+      exit(EXIT_SUCCESS);
+      break;
+    case 'G':
+    case 'g':
+      if(!animation_ongoing){
+	animation_ongoing = 1;
+	glutTimerFunc(TIMER_INTERVAL, on_timer, TIMER_ID);
+      std::cout << "pritisnuto g" << std::endl;
+      }
+      break;
+    case '4':
+      if(gRed.m_x >= 0.32){
+	gRed.m_x -=POMERAJ;
+      }
+      break;
+    case '6':
+      if(gRed.m_x < 0.86){
+	gRed.m_x +=POMERAJ;
+      }
+      break;
+    case 'd':
+    case 'D':      
+      if(gBlue.m_x <= -0.32){
+	gBlue.m_x +=POMERAJ;
+      }
+      break;
+    case 'a':
+    case 'A':
+      if(gBlue.m_x >= -0.86){
+	gBlue.m_x -=POMERAJ;
+      }
+      break;
+  }
+  glutPostRedisplay();
 }
